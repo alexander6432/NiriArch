675e078934a7be274f07d59d641100cda37be12c
Alexander Gallardo <alexander6432@gmail.com>
2026-08-24 19:09:04 -0600

    actializacion de yazi

--- hypr/hyprlua/look_and_feel.lua
+++ hypr/hyprlua/look_and_feel.lua
@@ -4,102 +4,100 @@
 
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
-  general = {
-    gaps_in                 = 4,
-    gaps_out                = 8,
-    float_gaps              = 8,
-    gaps_workspaces         = 4,
+	general = {
+		gaps_in = 4,
+		gaps_out = 8,
+		float_gaps = 8,
+		gaps_workspaces = 4,
 
-    border_size             = 3,
+		border_size = 3,
 
-    col                     = {
-      active_border   = {
-        colors = {
-          "rgba(33ccffee)",
-          "rgba(00ff99ee)",
-        },
-        angle = 45
-      },
-      inactive_border = "rgba(595959aa)",
-    },
+		col = {
+			active_border = {
+				colors = {
+					"rgba(33ccffee)",
+					"rgba(00ff99ee)",
+				},
+				angle = 45,
+			},
+			inactive_border = "rgba(595959aa)",
+		},
 
-    resize_on_border        = true,
-    no_focus_fallback       = true,
-    extend_border_grab_area = 8,
-    hover_icon_on_border    = true,
-    resize_corner           = 0,
+		resize_on_border = true,
+		no_focus_fallback = true,
+		extend_border_grab_area = 8,
+		hover_icon_on_border = true,
+		resize_corner = 0,
 
-    -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
-    allow_tearing           = false,
-    modal_parent_blocking   = true,
+		-- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+		allow_tearing = false,
+		modal_parent_blocking = true,
 
-    layout                  = "dwindle",
-    locale                  = "es",
+		layout = "dwindle",
+		locale = "es",
 
-    snap                    = {
-      enabled        = true,
-      window_gap     = 8,
-      monitor_gap    = 8,
-      border_overlap = true,
-      respect_gaps   = true,
-    }
+		snap = {
+			enabled = true,
+			window_gap = 8,
+			monitor_gap = 8,
+			border_overlap = true,
+			respect_gaps = true,
+		},
+	},
 
-  },
+	decoration = {
+		rounding = 8,
+		rounding_power = 4.0,
 
-  decoration = {
-    rounding              = 8,
-    rounding_power        = 4.0,
+		active_opacity = 1.0,
+		inactive_opacity = 0.9,
+		fullscreen_opacity = 1.0,
 
-    active_opacity        = 1.0,
-    inactive_opacity      = 0.9,
-    fullscreen_opacity    = 1.0,
+		dim_modal = true,
+		dim_inactive = true,
+		dim_strength = 0.2,
+		dim_special = 0.4,
+		dim_around = 0.4,
 
-    dim_modal             = true,
-    dim_inactive          = true,
-    dim_strength          = 0.2,
-    dim_special           = 0.4,
-    dim_around            = 0.4,
+		border_part_of_window = true,
 
-    border_part_of_window = true,
+		shadow = {
+			enabled = true,
+			range = 4,
+			render_power = 2,
+			sharp = false,
+			color = "rgba(1a1a1aee)",
+			color_inactive = "rgba(1a1a1aee)",
+			offset = { 0, 0 },
+			scale = 1.0,
+		},
 
-    shadow                = {
-      enabled        = true,
-      range          = 4,
-      render_power   = 2,
-      sharp          = false,
-      color          = "rgba(1a1a1aee)",
-      color_inactive = "rgba(1a1a1aee)",
-      offset         = { 0, 0 },
-      scale          = 1.0,
-    },
+		blur = {
+			enabled = true,
+			size = 8,
+			passes = 2,
+			ignore_opacity = true,
+			xray = false,
 
-    blur                  = {
-      enabled                   = true,
-      size                      = 4,
-      passes                    = 2,
-      ignore_opacity            = true,
-      xray                      = false,
+			noise = 0.02,
+			contrast = 0.9,
+			brightness = 0.8,
+			vibrancy = 0.2,
+			vibrancy_darkness = 0.4,
 
-      noise                     = 0.02,
-      contrast                  = 0.9,
-      brightness                = 0.8,
-      vibrancy                  = 0.2,
-      vibrancy_darkness         = 0.4,
+			special = false,
+			popups = true,
+			popups_ignorealpha = 0.25,
+			input_methods = true,
+			input_methods_ignorealpha = 0.25,
+		},
 
-      special                   = false,
-      popups                    = true,
-      popups_ignorealpha        = 0.25,
-      input_methods             = true,
-      input_methods_ignorealpha = 0.25,
-
-    },
-
-    glow                  = {
-      enabled        = true,
-      range          = 4,
-      render_power   = 2,
-      color          = "rgba(1a1a1aee)",
-      color_inactive = "rgba(1a1a1aee)",
-    }
-  },
+		glow = {
+			enabled = true,
+			range = 4,
+			render_power = 2,
+			color = "rgba(1a1a1aee)",
+			color_inactive = "rgba(1a1a1aee)",
+		},
+	},
 })
---
+++ nvim/lua/matugen.lua
@@ -0,0 +1,51 @@
+ local M = {}
+
+function M.setup()
+  require('base16-colorscheme').setup({
+    base00 = '#000000',
+    base01 = '#191615',
+    base02 = '#231f1e',
+    base03 = '#76706c',
+    base04 = '#ebdbb2',
+    base05 = '#fbf1c7',
+    base06 = '#fbf1c7',
+    base07 = '#fbf1c7',
+    base08 = '#fb4934',
+    base09 = '#83a598',
+    base0A = '#fabd2f',
+    base0B = '#b8bb26',
+    base0C = '#96e9c9',
+    base0D = '#e8e995',
+    base0E = '#fcd782',
+    base0F = '#7a0f02',
+  })
+
+  local hi = function(group, opts)
+    vim.api.nvim_set_hl(0, group, opts)
+  end
+
+  hi('TelescopeNormal',         { fg = '#fbf1c7',          bg = '#000000' })
+  hi('TelescopeBorder',         { fg = '#76706c',             bg = '#000000' })
+  hi('TelescopePromptNormal',   { fg = '#fbf1c7',          bg = '#000000' })
+  hi('TelescopePromptBorder',   { fg = '#76706c',             bg = '#000000' })
+  hi('TelescopePromptPrefix',   { fg = '#b8bb26',             bg = '#000000' })
+  hi('TelescopePromptCounter',  { fg = '#ebdbb2',  bg = '#000000' })
+  hi('TelescopePromptTitle',    { fg = '#000000',             bg = '#b8bb26' })
+  hi('TelescopePreviewTitle',   { fg = '#000000',             bg = '#fabd2f' })
+  hi('TelescopeResultsTitle',   { fg = '#000000',             bg = '#83a598' })
+  hi('TelescopeSelection',      { fg = '#fbf1c7',          bg = '#231f1e' })
+  hi('TelescopeSelectionCaret', { fg = '#b8bb26',             bg = '#231f1e' })
+  hi('TelescopeMatching',       { fg = '#b8bb26',             bold = true })
+end
+
+ -- Register a signal handler for SIGUSR1 (matugen updates)
+ local signal = vim.uv.new_signal()
+ signal:start(
+   'sigusr1',
+   vim.schedule_wrap(function()
+     package.loaded['matugen'] = nil
+     require('matugen').setup()
+   end)
+ )
+
+ return M
